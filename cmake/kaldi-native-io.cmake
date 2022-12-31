c6bcea159cc57e1ea8dfd9056eae2f896c8b4550
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-31 17:58:06 +0800

    Add kaldi_native_io

---
+++ cmake/kaldi-native-io.cmake
@@ -0,0 +1,39 @@
+function(download_kaldi_native_io)
+  include(FetchContent)
+
+  set(kaldi_native_io_URL  "https://github.com/csukuangfj/kaldi_native_io/archive/refs/tags/v1.17.2.tar.gz")
+  set(kaldi_native_io_HASH "SHA256=f916f2d3cd4c155b22cb64aa0d5e4f533b3ba2a40a77137c46506cfa7a00ec12")
+
+  # If you don't have access to the internet, please download it to your
+  # local drive and modify the following line according to your needs.
+  set(kaldi_native_io_URL  "file:///Users/fangjun/Downloads/kaldi_native_io-1.17.2.tar.gz")
+
+  set(KALDI_NATIVE_IO_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+  set(KALDI_NATIVE_IO_BUILD_PYTHON OFF CACHE BOOL "" FORCE)
+
+  FetchContent_Declare(kaldi_native_io
+    URL               ${kaldi_native_io_URL}
+    URL_HASH          ${kaldi_native_io_HASH}
+  )
+
+  FetchContent_GetProperties(kaldi_native_io)
+  if(NOT kaldi_native_io_POPULATED)
+    message(STATUS "Downloading kaldi_native_io ${kaldi_native_io_URL}")
+    FetchContent_Populate(kaldi_native_io)
+  endif()
+  message(STATUS "kaldi_native_io is downloaded to ${kaldi_native_io_SOURCE_DIR}")
+  message(STATUS "kaldi_native_io's binary dir is ${kaldi_native_io_BINARY_DIR}")
+
+  add_subdirectory(${kaldi_native_io_SOURCE_DIR} ${kaldi_native_io_BINARY_DIR} EXCLUDE_FROM_ALL)
+
+  target_include_directories(kaldi_native_io_core
+    PUBLIC
+      ${kaldi_native_io_SOURCE_DIR}/
+  )
+
+  set_target_properties(kaldi_native_io_core PROPERTIES OUTPUT_NAME "kaldi-hgmm-gmm-kaldi-native-io-core")
+
+  install(TARGETS kaldi_native_io_core DESTINATION lib)
+endfunction()
+
+download_kaldi_native_io()
--- kaldi-hmm-gmm/csrc/hmm-topology.cc
+++ kaldi-hmm-gmm/csrc/hmm-topology.cc
@@ -1,10 +1,17 @@
-// kaldi-hmm-gmm/csrc/hmm-topology.h
+// kaldi-hmm-gmm/csrc/hmm-topology.cc
 //
 // Copyright (c)  2022  Xiaomi Corporation
 
 // This file is copied and modified from
 // kaldi/src/hmm/hmm-topology.cc
 #include "kaldi-hmm-gmm/csrc/hmm-topology.h"
+
+#include "kaldi_native_io/csrc/io-funcs.h"
+
 namespace khg {
-HmmTopology::HmmTopology() = default;
+
+void HmmTopology::Read(std::istream &is, bool binary) {
+  kaldiio::ExpectToken(is, binary, "<Topology>");
 }
+
+}  // namespace khg
