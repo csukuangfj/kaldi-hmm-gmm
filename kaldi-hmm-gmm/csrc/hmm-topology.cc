70e88cf00cc4bdc4291d824a023fe1f6f4075a14
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-31 17:38:21 +0800

    Begin to add hmm topology

---
+++ kaldi-hmm-gmm/csrc/hmm-topology.cc
@@ -0,0 +1,10 @@
+// kaldi-hmm-gmm/csrc/hmm-topology.h
+//
+// Copyright (c)  2022  Xiaomi Corporation
+
+// This file is copied and modified from
+// kaldi/src/hmm/hmm-topology.cc
+#include "kaldi-hmm-gmm/csrc/hmm-topology.h"
+namespace khg {
+HmmTopology::HmmTopology() = default;
+}
---
+++ kaldi-hmm-gmm/csrc/hmm-topology.h
@@ -0,0 +1,21 @@
+// kaldi-hmm-gmm/csrc/hmm-topology.h
+//
+// Copyright (c)  2022  Xiaomi Corporation
+#ifndef KALDI_HMM_GMM_CSRC_HMM_TOPOLOGY_H_
+#define KALDI_HMM_GMM_CSRC_HMM_TOPOLOGY_H_
+
+// this file is copied and modified from
+// kaldi/src/hmm/hmm-topology.h
+
+namespace khg {
+
+class HmmTopology {
+ public:
+  HmmTopology();
+
+ private:
+};
+
+}  // namespace khg
+
+#endif  // KALDI_HMM_GMM_CSRC_HMM_TOPOLOGY_H_
