d6a57925cc747e66be730654274021388254d9db
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-05 15:09:32 +0800

    small fixes

--- cmake/kaldifst.cmake
+++ cmake/kaldifst.cmake
@@ -1,24 +1,18 @@
 function(download_kaldifst)
   include(FetchContent)
 
-  set(kaldifst_URL  "https://github.com/k2-fsa/kaldifst/archive/refs/tags/v1.7.0.tar.gz")
-  set(kaldifst_URL2 "https://huggingface.co/csukuangfj/kaldi-hmm-gmm-cmake-deps/resolve/main/kaldifst-1.7.0.tar.gz")
-  set(kaldifst_HASH "SHA256=d5f4adbf7634e8cea57da00981e9f6424e777ad0396ab8f6f52baac0ceffb11b")
-
-  set(kaldifst_URL "https://github.com/csukuangfj/kaldifst/archive/f330e9f86610bf5ad35f02541ae6c635d9e788c0.zip")
-  set(kaldifst_URL2 "")
-  set(kaldifst_HASH "")
+  set(kaldifst_URL  "https://github.com/k2-fsa/kaldifst/archive/refs/tags/v1.7.4.tar.gz")
+  set(kaldifst_URL2 "https://huggingface.co/csukuangfj/kaldi-hmm-gmm-cmake-deps/resolve/main/kaldifst-1.7.4.tar.gz")
+  set(kaldifst_HASH "SHA256=91c4a5fe3fe5d65a26bd232d1fe727712ed839df1c01e56ce37705f886ef2b48")
 
   # If you don't have access to the Internet,
   # please pre-download kaldi_native_io
   set(possible_file_locations
-    $ENV{HOME}/Downloads/kaldifst-f330e9f86610bf5ad35f02541ae6c635d9e788c0.zip
-
-    $ENV{HOME}/Downloads/kaldifst-1.7.0.tar.gz
-    ${PROJECT_SOURCE_DIR}/kaldifst-1.7.0.tar.gz
-    ${PROJECT_BINARY_DIR}/kaldifst-1.7.0.tar.gz
-    /tmp/kaldifst-1.7.0.tar.gz
-    /star-fj/fangjun/download/github/kaldifst-1.7.0.tar.gz
+    $ENV{HOME}/Downloads/kaldifst-1.7.4.tar.gz
+    ${PROJECT_SOURCE_DIR}/kaldifst-1.7.4.tar.gz
+    ${PROJECT_BINARY_DIR}/kaldifst-1.7.4.tar.gz
+    /tmp/kaldifst-1.7.4.tar.gz
+    /star-fj/fangjun/download/github/kaldifst-1.7.4.tar.gz
   )
 
   foreach(f IN LISTS possible_file_locations)
